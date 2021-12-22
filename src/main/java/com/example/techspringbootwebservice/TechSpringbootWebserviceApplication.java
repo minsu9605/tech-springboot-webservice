610395314ce370db967ea821bae1dc74ab8edac1
minsu9605 <alstn9605@naver.com>
2021-12-22 13:26:30 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/techspringbootwebservice/TechSpringbootWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.techspringbootwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSpringbootWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechSpringbootWebserviceApplication.class, args);
+    }
+
+}
